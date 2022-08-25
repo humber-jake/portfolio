34228c8213c4c3b689b1925446e4253912347c1f
humber-jake <humber.jake@gmail.com>
2022-08-25 12:38:57 -0700

    minor style tweaks and bug fixes

--- src/Project.js
+++ src/Project.js
@@ -6,17 +6,15 @@ function Project(props) {
     const {name, tech, description, link } = props;
 
     return (
+        <a className='ProjectLink' href={link}>
         <div className='Project'>
             <div className='ProjectName'>{name}</div>
             <div className='ProjectTech'>{tech.join(', ')}</div>
-            <a className='ProjectLink' href={link}>
                 <img className='ProjectThumbnail' src={`thumbnails/${name}.png`} alt=''></img>
-            </a>
             <div className='ProjectDescription'>{description}</div>
-            <a className='ProjectLink' href={link}>
                 <EastIcon/>
-            </a>
         </div>
+        </a>
     );
 }
 
